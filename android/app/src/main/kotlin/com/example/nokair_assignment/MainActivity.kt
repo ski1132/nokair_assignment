e4e3449bc1bfe373c60f03a633d1377bd8629528
Ski <rizkee2541@gmail.com>
2026-02-22 22:21:11 +0700

    commit main

---
+++ android/app/src/main/kotlin/com/example/nokair_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.nokair_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/ski/fvm/versions/3.38.5"
-export "FLUTTER_APPLICATION_PATH=/Users/ski/Desktop/project/nokair_assigment"
+export "FLUTTER_APPLICATION_PATH=/Users/ski/Desktop/project/nokair_assignment"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
